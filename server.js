2f4128e6ffa020184032f8e05bbc1031f75d4965
speedymcfan <113385359+speedymcfan@users.noreply.github.com>
2023-11-30 01:58:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
+
 
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://anandmure:pzfcvb123@swe432.tt2c69k.mongodb.net/?retryWrites=true&w=majority";
@@ -18,12 +21,15 @@ async function main(){
 }
 
 
-main().catch(console.error);
+client.connect();
+
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-app.get('/song', function(req, res) {
+app.get('song', function(req, res) {
   const result = client.db("radio").collection("music").findOne({current: '1'});
   if(!result){
     console.log('failed');
@@ -33,50 +39,75 @@ app.get('/song', function(req, res) {
   }
 });
 
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.get('/', async (req, res) => {
+  const song = await client.db("radio").collection("music").findOne({ current: '1'});
+  res.render('pages/index', { song });
 });
 
-app.get('/index', function(req, res) {
-    res.render('pages/index');
+app.get('/index', async (req, res) => {
+  const song = await client.db("radio").collection("music").findOne({ current: '1'});
+  res.render('pages/index', { song });
 });
 
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+
+app.get('/songs', async (req, res) => {
+  const song1 = await client.db("radio").collection("music").findOne({ current: '1'});
+  const songs = await client.db("radio").collection("music").find({}).toArray();
+  res.render('pages/songs', { song1, songs });
 });
 
-app.get('/songs', function(req, res) {
-    res.render('pages/songs');
+app.get('/contact', async (req, res) => {
+  const song = await client.db("radio").collection("music").findOne({ current: '1'});
+  res.render('pages/contact', { song });
 });
 
-app.get('/contact', function(req, res) {
-    res.render('pages/contact');
-});
-
-app.get('/login', function(req, res) {
-  res.render('pages/login');
+app.get('/login', async(req, res) => {
+  const song = await client.db("radio").collection("music").findOne({ current: '1'});
+  res.render('pages/login', { song });
 });
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await client.db("radio").collection("music").findOne({ username });
+  const user = await client.db("radio").collection("logins").findOne({ username });
 
   if (!user) {
     res.status(401).send('Invalid username or password');
     return;
   }
 
-  const isPasswordValid = await compare(password, user.password);
+  const isPasswordInvalid = await password.localeCompare(user.password);
 
-  if (!isPasswordValid) {
+  if (isPasswordInvalid) {
     res.status(401).send('Invalid username or password');
     return;
   }
 
-  req.session.userId = user._id;
-  res.redirect('/');
+  if (!user.type.localeCompare("dj")) {
+    res.redirect('/dj-login');
+  } else {
+    res.redirect('/user-login');
+  }
+  
 });
+
+app.get('/dj-login', async (req, res) => {
+  const song1 = await client.db("radio").collection("music").findOne({ current: '1'});
+  const songs = await client.db("radio").collection("music").find({}).toArray();
+  res.render('pages/dj-login', { song1, songs });
+});
+
+app.post('/server.js', async (req, res) => {
+  const itemId = req.body.itemId;
+  await client.db("radio").collection("music").updateOne({ current: '1' }, {$set: {current: '0'}});
+  await client.db("radio").collection("music").updateOne({ title: itemId }, {$set: {current: '1'}});
+})
+
+app.get('/user-login', async (req, res) => {
+  const song = await client.db("radio").collection("music").findOne({ current: '1'});
+  res.render('pages/user-login', { song });
+});
+
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
